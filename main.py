7f38b09ef014afc039c20a73e8a043b273ad02d9
winter_x64 <75623356+winter-x64@users.noreply.github.com>
2024-10-09 10:48:52 +0000

    condition

---
+++ main.py
@@ -0,0 +1,9 @@
+number_1: int = int(input("Enter a number 1: "))
+number_2: int = int(input("Enter a number 2: "))
+
+if number_1 == number_2:
+    print("Both are same")
+elif number_1 > number_2:
+    print(number_1,"is greater")
+elif number_1 < number_2:
+    print(number_2,"is greater")
